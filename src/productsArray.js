08025b924de5ce7d30dcf275386b1827c5e5df44
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-05-21 12:24:37 +0500

    created productArr.js

--- src/productsArray.js
+++ src/productsArray.js
@@ -1,14 +1,222 @@
 const array = [
   {
     categoryId: 1,
-    category: '',
+    category: 'all',
     products: [
       {
         id: 1,
-        image: '',
-        desc: '',
-        price: ''
+        image: '../assets/images/ProductsCategoryImgs/allcategoryimg1.png',
+        desc: ' Seedra Cilantro Seeds for Planting Indoor and Outdoor ',
+        price: '$12.56'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/allcategoryimg2.jpg',
+        desc: 'SEEDRA Corn - Bodacious Hybrid Seeds for Indoor and Outdoor Planting',
+        price: '$12.56'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/allcategoryimg3.png',
+        desc: 'SEEDRA Spinach Seeds for Indoor and Outdoor Planting',
+        price: '$12.56'
+      }, 
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/allcategoryimg4.png',
+        desc: 'Seedra Cilantro Seeds for Planting Indoor and Outdoor',
+        price: '$12.56'
+      },
+      {
+        id: 5,
+        image: '../assets/images/ProductsCategoryImgs/allcategoryimg5.png',
+        desc: 'SEEDRA Corn - Bodacious Hybrid Seeds for Indoor and Outdoor Planting',
+        price: '$12.56',
+        sale: '$15.56'
+      },
+    ]
+  },  
+  {
+    categoryId: 2,
+    category: 'bundles',
+    products: [
+      {
+        id: 1,
+        image: '../assets/images/ProductsCategoryImgs/bundlecategoryimg1.webp',
+        desc: '6 Squash Seeds Variety Pack - 325+ Non GMO, Heirloom Seeds for Indoor Outdoor Home Garden - Summer and Winter Squash Seeds,',
+        price: '$16.16'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/bundlecategoryimg2.webp',
+        desc: '8 Onion Seeds Variety Pack - 200+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Walla Walla, price ',
+        price: '$17.17',
+        sale: '$16.16'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/bundlecategoryimg3.webp',
+        desc: 'Spinach Seeds Variety Pack - 305+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Bloomsdale',
+        price: '$16.16'
+      },
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/bundlecategoryimg4.jpg',
+        desc: '10 Lettuce Seeds Variety Pack - 2215+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Buttercrunch ,',
+        price: '$17.95',
+        sale: '$16.16'
       }
     ]
   },
+  {
+    categoryId: 3,
+    category: 'herbs',
+    products: [
+      {
+        id: 1,
+        image: '../assets/images/ProductsCategoryImgs/herbscategoryimg1.png',
+        desc: '600 English Lavender Seeds for Indoor and Outdoor Planting - 500mg - GMO-Free and Heirloom Seeds - Germination Above 90% - 1 Pack,',
+        price: '$17.65',
+        sale: '$16.16'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/herbscategoryimg2.webp',
+        desc: '300+ Cilantro Seeds for Indoor, Outdoor and Hydroponic Planting, Non GMO Heirloom Seeds for Home Garden - 1 Pack ',
+        price: '$16.16'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/herbscategoryimg3.webp',
+        desc: '300+ Italian Flat Leaf Parsley Seeds for Indoor, Outdoor and Hydroponic Planting, Non GMO Heirloom Seeds for Home Garden - 1 Pack',
+        price: '$16.16'
+      },
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/herbscategoryimg4.png',
+        desc: '300+ Italian Large Leaf Basil Seeds for Indoor, Outdoor and Hydroponic Planting, Non GMO Heirloom Seeds for Home Garden - 1 Pack ,',
+        price: '$15.25'
+      }
+    ]
+  },
+  {
+    categoryId: 4,
+    category: 'vegetables',
+    products: [
+      {
+        id: 1,
+        image: '../assets/images/ProductsCategoryImgs/vegetablescategoryimg1.webp',
+        desc: '6 Squash Seeds Variety Pack - 325+ Non GMO, Heirloom Seeds for Indoor Outdoor Home Garden - Summer and Winter Squash Seeds, Zucchini Seeds + Eggplant Seeds,,',
+        price: '$16.16'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/vegetablescategoryimg2.webp',
+        desc: ' Spinach Seeds Variety Pack - 305+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Bloomsdale',
+        price: '$16.16'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/vegetablescategoryimg3.webp',
+        desc: '11 Sweet and Hot Pepper Seeds Variety Pack - 730+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Cayenne',
+        price: '$16.16'
+      },
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/vegetablescategoryimg4.webp',
+        desc: '8 Onion Seeds Variety Pack - 200+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Walla Walla,',
+        price: '$15.16'
+      }
+    ]
+  },
+  {
+    categoryId: 5,
+    category: 'flowers',
+    products: [
+      {
+        id: 1,
+        image: '../assets/images/ProductsCategoryImgs/flowercategoryimg1.jpg',
+        desc: '999+ Marigold Seeds - Petite Mix for Indoor and Outdoor Planting, Non GMO Heirloom Seeds for Home Garden - 1 Pack',
+        price: '$26.25'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/flowerscategoryimg2.jpg',
+        desc: 'Wildflower Mix Seeds for Indoor and Outdoor Planting - 10 Grams - Heirloom Seeds -',
+        price: '$16.50'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/flowerscategoryimg3.jpg',
+        desc: 'Wildflower Mix Seeds for Indoor and Outdoor Planting, Non GMO Heirloom Seeds for Home Garden - 2.8 oz - 1 Pack, ',
+        price: '$18.99'
+      },
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/flowerscategoryimg4.jpg',
+        desc: 'Wildflower Mix Seeds for Indoor and Outdoor Planting, Non GMO Heirloom Seeds for Home Garden - 10 Grams - 1 Pack ,',
+        price: '$19.99'
+      }
+    ]
+  },
+  {
+    categoryId: 6,
+    category: 'fruits',
+    products: [
+      {
+        id: 1,
+        image: '../assets/images/ProductsCategoryImgs/fruitcategoryimg1.jpg',
+        desc: '10 Lettuce Seeds Variety Pack - 2215+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Buttercrunch',
+        price: '$24.41'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/fruitcategoryimg2.webp',
+        desc: 'Cucumber Seeds Variety Pack - 220+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - National Pickling-',
+        price: '$16.16'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/fruitcategoryimg3.webp',
+        desc: '7 Cabbage Seeds Variety Pack - 2245+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Golden & Red Acre,',
+        price: '$16.15'
+      },
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/fruitcategoryimg4.webp',
+        desc: '5 Beet Seeds Variety Pack - 530+ Non GMO, Heirloom Seeds for Indoor Outdoor Home Garden - Chioggia ',
+        price: '$20.15'
+      }
+    ]
+  },
+  {
+    categoryId: 6,
+    category: 'supplies',
+    products: [
+      {
+        id: 1,
+        image: '../assets/images/ProductsCategoryImgs/suppliesimg1.webp',
+        desc: '4 Bean Seeds Variety Pack - 125+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Royal Burgundy',
+        price: '$25.55'
+      },
+      {
+        id: 2,
+        image: '../assets/images/ProductsCategoryImgs/suppliesimg2.webp',
+        desc: 'Carrot Seeds Variety Pack - 1385+ Non GMO, Heirloom Seeds for Indoor Outdoor Hydroponic Home Garden - Chantenay Red Cored, Imperator, Scarlet Nantes, Solar Yellow-',
+        price: '$20.50'
+      },
+      {
+        id: 3,
+        image: '../assets/images/ProductsCategoryImgs/suppliesimg3.webp',
+        desc: '600 Lettuce Buttercrunch Seeds for Indoor and Outdoor Planting - GMO-Free and Heirloom Seeds - 1 Pack',
+        price: '$16.16'
+      },
+      {
+        id: 4,
+        image: '../assets/images/ProductsCategoryImgs/suppliesimg4.jpg',
+        desc: '00+ Okra Clemson Spineless Seeds for Indoor and Outdoor Planting, Non GMO Heirloom Seeds for Home Garden - 1 Pack',
+        price: '$7.90'
+      }
+    ]
+  }
 ]
