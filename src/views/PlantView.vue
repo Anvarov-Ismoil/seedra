2349395ab16660352b1398994ec857b58afd26a7
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-05-21 10:54:06 +0500

    that the plant page is finished without media

--- src/views/PlantView.vue
+++ src/views/PlantView.vue
@@ -13,64 +13,68 @@
             <i class="fa-solid fa-clock"></i>
             <h3>12.09.2021</h3>
          </div>
-         <div class="content flex">
+         <div class="content flex gap-6">
             <div class="left w-4/6">
-               <p>
+               <p class="font-light text-[16px] text-primary mt-8">
                Spinach is a cool-season plant that’s one of the first crops of spring and a great fall crop as well. You’ll enjoy growing this undemanding plant and will enjoy eating it even more. Harvest spinach any time to enjoy raw in salads or cook it up in seconds for a delicious side dish that’s loaded with vitamins and minerals. 
                If you want to grow spinach in your garden, here’s everything you need to know.
             </p>
-
+            <br>
             <p>
                You can also download my How Do I Grow Spinach? one-sheet and keep the free resource handy for your reference
             </p>
 
-            <img src="../assets/images/plants-content.svg" alt="">
+            <img src="../assets/images/plants-content.svg" class="mt-8 mb-14" alt="">
 
-            <h2>When, Where and How to Plant Spinach</h2>
+            <h2 class="font-semibold text-3xl text-primary mb-6">When, Where and How to Plant Spinach</h2>
 
             <p>
                Spinach can be either sown directly into the garden or started from seeds indoors. For the least work and the most convenience, seedlings can also be purchased from a nursery in spring.
                <br><br>
 To give spinach a head start, plant seeds in sterile seed-starting mix indoors about six weeks before the last expected frost date. The seeds will germinate one to two weeks later in soil that is between 60 and 68 degrees. If growing in a room that stays cool, consider using a seed-starting mat that will raise the temperature of the soil.
-<br><br>
+<br><br><br>
 Spinach is frost tolerant, so it can go in the ground long before many other crops. When spinach seedlings have two true leaves and when there are four or fewer weeks remaining until your last frost date, the seedlings may be transplanted outdoors.  
-<br><br>
+<br><br><br>
 
 Seeds can be planted outdoors as soon as the soil is workable in spring — about six weeks before the last expected frost — and they will germinate as the days warm.
 <br><br>
 To germinate seeds faster and more reliably, there is a process called “priming.” A week before sowing spinach seeds indoors or out, soak seeds in room temperature water overnight or up to 24 hours. Next, place the seeds on a paper towel to air dry for one or two days. Once the seeds appear dry, place them in an airtight container and store the container in a cool place. The seeds will have soaked up and retained enough water to stimulate the first stages of germination. Wait at least five days, but no more than seven, and sow the seeds. 
-<br><br>
+<br><br><br>
 Primed spinach seeds will germinate both faster and more uniformly: In about five days, the grass-like seedlings will emerge. 
 
 
             </p>
             </div>
             <div class="right w-4/12">
-               <h2>Related titles</h2>
-               <div class="title1 bg-light-blue">
+               <h2 class="font-semibold text-[24px] mb-5">Related titles</h2>
+               <div class="title1 bg-light-blue rounded-xl pl-6 pt-6 mb-6">
                   <div class="time flex items-center gap-2 text-secondary">
                      <i class="fa-solid fa-clock"></i>
                      <h3>12.09.2021</h3>
                   </div>
 
                   <div class="contentTitle">
-                     <h2>How to plant spinach correctly in winter</h2>
-                     <p>In most areas, successive sowing can be done from early spring until early winter, but more often during hotter months...</p>
-                     <button class="btn">Read</button>
-                     <img src="../assets/images/plant-title.svg" alt="">
+                     <h2 class="text-primary font-semibold text-2xl mt-2 mb-1">How to plant spinach correctly in winter</h2>
+                     <p class="text-secondary font-normal mb-3">In most areas, successive sowing can be done from early spring until early winter, but more often during hotter months...</p>
+                     <button class="btn bg-white py-3 px-8 text-brand-green rounded-lg">Read</button>
+                     <div class="img flex justify-end">
+                        <img src="../assets/images/plant-title.svg" alt="">
+                     </div>
                   </div>
                </div>
-               <div class="title2 bg-brand-green">
+               <div class="title2 bg-brand-green/10 rounded-xl pl-6 pt-6 relative">
                   <div class="time flex items-center gap-2 text-secondary">
                      <i class="fa-solid fa-clock"></i>
                      <h3>12.09.2021</h3>
                   </div>
 
                   <div class="contentTitle">
-                     <h2>How to plant spinach correctly in winter</h2>
-                     <p>In most areas, successive sowing can be done from early spring until early winter, but more often during hotter months...</p>
-                     <button class="btn">Read</button>
-                     <img src="../assets/images/plant-title2.png" alt="">
+                     <h2 class="text-primary font-semibold text-2xl mt-2 mb-1">How to plant spinach correctly in winter</h2>
+                     <p class="text-secondary font-normal mb-3">In most areas, successive sowing can be done from early spring until early winter, but more often during hotter months...</p>
+                     <button class="btn bg-white py-3 px-8 text-brand-green rounded-lg">Read</button>
+                     <div class="img flex justify-end">
+                        <img src="../assets/images/plant-title2.png" alt="">
+                     </div>
                   </div>
                </div>
             </div>
@@ -78,8 +82,8 @@ Primed spinach seeds will germinate both faster and more uniformly: In about fiv
 
          <div class="navigate flex justify-between bg-brand-green mt-5 p-5 rounded-2xl">
             <div class="text flex flex-col">
-               <h3>Title was helpful?</h3>
-               <h2>Share with friends</h2>
+               <h3 class="font-medium text-white text-[16px]">Title was helpful?</h3>
+               <h2 class="font-semibold text-white text-[24px]">Share with friends</h2>
             </div>   
 
             <div class="icons flex gap-4">
